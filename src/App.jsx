f916b5678a4277f042552aaa63c52a24934a48f7
Wadad Parker <wadadparker5@gmail.com>
2025-01-19 01:25:09 +0530

    feat: Context Wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,22 @@ import Title from "./components/Title";
 
 export const CountContext = createContext();
 
-function App() {
+const ApplyContextComponent = () => {
   const[count,setCount] = useState(0);
+  return (
+      <CountContext.Provider value={{count,setCount}}>
+        <Sidebar />
+        <Title />
+      </CountContext.Provider>
+  )
+}
+
+function App() {
   
   return (
       <div className="flex justify-center items-start p-40 bg-black h-screen space-x-20">
-        <CountContext.Provider value={{count,setCount}}>
-          <Sidebar />
-          <Title />
-        </CountContext.Provider>
+        <h1 className="text-4xl absolute text-white top-10">This heading does not rerender</h1>
+        <ApplyContextComponent />
       </div>
   )
 }
