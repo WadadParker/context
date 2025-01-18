e8fba39a10cd6ccf6c9eaef43e8dd81acfe42441
Wadad Parker <wadadparker5@gmail.com>
2025-01-18 19:49:24 +0530

    refactor: some lines

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react"
+
 import Sidebar from "./components/Sidebar"
 import Title from "./components/Title";
+
 function App() {
   const[count,setCount] = useState(0);
+
   return (
       <div className="flex justify-center items-start p-40 bg-black h-screen space-x-20">
         <Sidebar count={count} setCount={setCount}/>
