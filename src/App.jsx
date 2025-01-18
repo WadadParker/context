4373413e58407c45cc63a594dd7dd9c3b487a046
Wadad Parker <wadadparker5@gmail.com>
2025-01-18 23:49:48 +0530

    refactor: add context

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import { useState } from "react"
+import { createContext, useState } from "react"
 
 import Sidebar from "./components/Sidebar"
 import Title from "./components/Title";
 
+export const CountContext = createContext();
+
 function App() {
   const[count,setCount] = useState(0);
-
+  
   return (
       <div className="flex justify-center items-start p-40 bg-black h-screen space-x-20">
-        <Sidebar count={count} setCount={setCount}/>
-        <Title setCount={setCount}/>
+        <CountContext.Provider value={{count,setCount}}>
+          <Sidebar />
+          <Title />
+        </CountContext.Provider>
       </div>
   )
 }
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,30 +1,35 @@
+import { useContext } from "react"
+import { CountContext } from "../App"
 
-const AddButton = ({setCount}) => {
+const AddButton = () => {
+    const {setCount} = useContext(CountContext);
     return <button className="text-lg border rounded-lg text-green-400 px-6 py-2 transition-all ease-linear focus:translate-y-[1px]" onClick={()=>setCount(prev=>prev + 1)}>Add</button>
 }
 
-const SubtractButton = ({setCount}) => {
+const SubtractButton = () => {
+    const {setCount} = useContext(CountContext);
     return <button className="text-lg border rounded-lg text-blue-400 px-6 py-2 transition-all ease-linear focus:translate-y-[1px]" onClick={()=>setCount(prev=>prev - 1)}>Subtract</button>
 }
 
-const ButtonContainer = ({setCount}) => {
+const ButtonContainer = () => {
+
     return (
         <div className="flex w-28 h-20 items-center gap-x-4">
-            <AddButton setCount={setCount}/>
-            <SubtractButton setCount={setCount} />
+            <AddButton />
+            <SubtractButton  />
         </div>
     )
 }
 
 const Heading = () => {
-    return <h1 className="text-6xl text-red-400">Prop Drilling</h1>
+    return <h1 className="text-6xl text-red-400">With Context</h1>
 }
 
-const Title = ({setCount}) => {
+const Title = () => {
   return (
     <div className="flex flex-col space-y-4">
     <Heading />
-    <ButtonContainer setCount={setCount}/>
+    <ButtonContainer />
     </div>
   )
 }
